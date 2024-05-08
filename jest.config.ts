0140b413bed51c9e58cc9ed368472630f13aa3b0
Heber Senger <sengerhe@gmail.com>
2024-05-08 17:10:29 -0300

    set timeout for jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,11 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
+const ONE_SECOND = 1000;
+
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   verbose: false,
+  testTimeout: ONE_SECOND,
 };
 
 export default jestConfig;
--- src/projects/update.spec.ts
+++ src/projects/update.spec.ts
@@ -3,5 +3,6 @@ import { updateProject } from "./update";
 it("updates the version", () => {
   const project = { name: "Test", version: 1 };
   const updated = updateProject(project);
+
   expect(updated.version).toBe(2);
 });
